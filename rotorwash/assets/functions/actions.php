7a213cfbddfade9b637219af2f0ffb75c8888b1a
Jason Lengstorf <jason.lengstorf@copterlabs.com>
2012-01-22 21:58:12 -0800

    Cleaned up output and added documentation.

--- rotorwash/assets/functions/actions.php
+++ rotorwash/assets/functions/actions.php
@@ -2,15 +2,16 @@
 /**
  * RotorWash actions
  *
- * @package WordPress
- * @subpackage RotorWash
- * @since RotorWash 1.0
+ * @package     WordPress
+ * @subpackage  RotorWash
+ * @since       1.0
  */
 
 /**
  * Adds Facebook root to the footer of the site.
  * 
- * @return void
+ * @return  void
+ * @since   1.0.2
  */
 function rw_add_fb_root(  )
 {
@@ -24,6 +25,8 @@ function rw_add_fb_root(  )
             );
     }
 ?>
+
+<!-- Initializes Facebook ("Like" buttons and such) -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -32,9 +35,33 @@ function rw_add_fb_root(  )
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<?php echo $opts['fb_app_id']; ?>";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+
 <?php
 }
 add_action('wp_footer', 'rw_add_fb_root');
+
+/**
+ * Adds Google +1 button to the footer of the site
+ * 
+ * @return  void
+ * @since   1.0.5
+ */
+function rw_add_gplus_root(  )
+{
+?>
+
+<!-- Initializes Google +1 Buttons -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+<?php
+}
+add_action('wp_footer', 'rw_add_gplus_root');
 
 /**
  * Adds Facebook Open Graph tags to the website using the wp_head action.
@@ -42,10 +69,9 @@ add_action('wp_footer', 'rw_add_fb_root');
  * Depending on what type of post/page we're displaying, different information will
  * be used. This makes for those pretty-looking shared links in the Facebook timeline.
  * 
- * @see http://codex.wordpress.org/Plugin_API/Action_Reference/wp_head
- * @since RotorWash 1.0
- *
- * @return void
+ * @see     http://codex.wordpress.org/Plugin_API/Action_Reference/wp_head
+ * @since   1.0.2
+ * @return  void
  */
 function rw_add_fb_og_tags()
 {
@@ -115,12 +141,11 @@ add_action('wp_head', 'rw_add_fb_og_tags');
 /**
  * Adds the HTML5shiv script to the head
  * 
- * @return void
- * @since RotorWash 1.0.1
+ * @return  void
+ * @since   1.0.1
  */
 function rw_add_html5shiv(  )
 {
-
 ?>
 
 <!--[if lt IE 9]>
@@ -128,15 +153,14 @@ function rw_add_html5shiv(  )
 <![endif]-->
 
 <?php
-
 }
 add_action('wp_head', 'rw_add_html5shiv');
 
 /**
  * Enqueues scripts for the theme
  * 
- * @return void
- * @since RotorWash 1.0
+ * @return  void
+ * @since   1.0
  */
 function rw_enqueue_scripts(  )
 {
@@ -149,6 +173,9 @@ add_action('wp_enqueue_scripts', 'rw_enqueue_scripts');
 
 /**
  * Adds the theme settings button to the dashboard
+ * 
+ * @return  void
+ * @since   1.0.2
  */
 function rw_create_menu_item(  )
 {
@@ -161,6 +188,12 @@ function rw_create_menu_item(  )
 }
 add_action('admin_menu', 'rw_create_menu_item');
 
+/**
+ * Registers the custom settings with WordPress
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
 function register_custom_settings(  )
 {
 	register_setting('rw-theme-settings', 'rw_theme_settings');
@@ -186,66 +219,152 @@ function register_custom_settings(  )
     add_settings_field('paypal_currency', 'Choose Your Currency', 'rw_paypal_currency', 'rw-theme-settings', 'rw-paypal-settings', array('label_for'=>'paypal_currency'));
 }
 
+/**
+ * Adds a message for the top of the theme settings
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
 function rw_theme_settings_text(  )
 {
-    echo '<p>Add a default image for sharing on Facebook and other fun stuff.</p>';
+?>
+
+<p>Add a default image for sharing on Facebook and other fun stuff.</p>
+
+<?php
 }
 
+/**
+ * Sets a default image for Facebook shares, Gravatars, etc.
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
 function rw_default_image(  )
 {
     rw_text_field(__FUNCTION__);
 }
 
+/**
+ * Shows Google Analytics section text
+ * 
+ * @return  void
+ * @since   1.0.4
+ */
 function rw_ga_settings_text(  )
 {
-    echo '<p>Keep track of your site traffic and stats: '
-        . '<a href="http://www.google.com/analytics/">sign up for Google Analytics</a></p>'
-        . '<p>Your Analytics ID will look something like this: UA-12345678-9</p>';
+?>
+
+<p>
+    Keep track of your site traffic and stats: 
+    <a href="http://www.google.com/analytics/">sign up for Google Analytics</a>
+</p>
+<p>Your Analytics ID will look something like this: UA-12345678-9</p>
+
+<?php
 }
 
+/**
+ * Sets the Google Analytics ID
+ * 
+ * @return  void
+ * @since   1.0.4
+ */
 function rw_ga_id(  )
 {
     rw_text_field(__FUNCTION__);
 }
 
+/**
+ * Shows text for the Facebook settings section
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
 function rw_fb_settings_text(  )
 {
-    echo '<p>Facebook settings. These are for comment administration and other good stuff.</p>'
-        . '<p><a href="https://developers.facebook.com/apps/">Register your site with Facebook to get its app ID.</a></p>'
-        . '<p>To get the Facebook admin ID(s), go to '
-        . '<a href="https://graph.facebook.com/copterlabs">https://graph.facebook.com/copterlabs</a> '
-        . 'and replace "copterlabs" with your Facebook username(s). Multiple values must be comma-separated.</p>';
+?>
+
+<p>Facebook settings. These are for comment administration and other good stuff.</p>
+<p><a href="https://developers.facebook.com/apps/">Register your site with Facebook to get its app ID.</a></p>
+<p>
+    To get the Facebook admin ID(s), go to 
+    <a href="https://graph.facebook.com/copterlabs">https://graph.facebook.com/copterlabs</a>
+    and replace "copterlabs" with your Facebook username(s). Multiple values must be comma-separated.
+</p>
+
+<?php
 }
 
+/**
+ * Adds the App ID text field
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
 function rw_fb_app_id(  )
 {
     rw_text_field(__FUNCTION__);
 }
 
+/**
+ * Adds the Facebook admins text field
+ * 
+ * @return  void
+ * @since   1.0.2
+ */
 function rw_fb_admins(  )
 {
     rw_text_field(__FUNCTION__);
 }
 
+/**
+ * Adds the top text to the PayPal settings section
+ * 
+ * @return  void
+ * @since   1.0.3
+ */
 function rw_paypal_settings_text(  )
 {
-    echo '<p>These settings are required if you plan to place a donation button on the site.</p>'
-        . '<p>The PayPal Email is where donations will be sent. This should be tied to a valid PayPal account.</p>'
-        . '<p>The post ID from which the donation was made is used as the item number for the sake of tracking.</p>'
-        . '<p>Supply a title for the form to tell the reader <em>why</em> they should donate (i.e. <em>Buy Me a Cup of Coffee</em>)</p>'
-        . '<p>The item description shows up on the PayPal checkout page.</p>';
+?>
+
+<p>These settings are required if you plan to place a donation button on the site.</p>
+<p>The PayPal Email is where donations will be sent. This should be tied to a valid PayPal account.</p>
+<p>The post ID from which the donation was made is used as the item number for the sake of tracking.</p>
+<p>Supply a title for the form to tell the reader <em>why</em> they should donate (i.e. <em>Buy Me a Cup of Coffee</em>)</p>
+<p>The item description shows up on the PayPal checkout page.</p>
+
+<?php
 }
 
+/**
+ * Adds the PayPal email address to the settings
+ * 
+ * @return  void
+ * @since   1.0.3
+ */
 function rw_paypal_addr(  )
 {
     rw_text_field(__FUNCTION__);
 }
 
+/**
+ * Adds the item code to the settings
+ * 
+ * @return  void
+ * @since   1.0.3
+ */
 function rw_paypal_item(  )
 {
     rw_text_field(__FUNCTION__);
 }
 
+/**
+ * Adds the currency type to the settings
+ * 
+ * @return  void
+ * @since   1.0.3
+ */
 function rw_paypal_currency(  )
 {
     $opts = get_option('rw_theme_settings');
@@ -287,19 +406,45 @@ function rw_paypal_currency(  )
 
 }
 
+/**
+ * Adds the text to the settings area
+ * 
+ * @return  void
+ * @since   1.0.3
+ */
 function rw_paypal_title(  )
 {
     rw_text_field(__FUNCTION__);
 }
 
+/**
+ * Creates a text field for the Theme settings page
+ * 
+ * @return  void
+ * @since   1.0.1
+ */
 function rw_text_field( $func )
 {
     $field = str_replace('rw_', '', $func);
     $opts = get_option('rw_theme_settings');
     $value = isset($opts[$field]) ? $opts[$field] : '';
-    echo '<input id="' . $field . '" name="rw_theme_settings[' . $field . ']" size="40" type="text" value="' . $value . '" />';
+?>
+
+<input id="<?php echo $field; ?>" 
+       name="rw_theme_settings[<?php echo $field; ?>]" 
+       size="40" 
+       type="text" 
+       value="<?php echo $value; ?>" />
+
+<?php
 }
 
+/**
+ * Loads the custom theme settings area
+ * 
+ * @return  void
+ * @since   1.0.1
+ */
 function rw_settings_page(  )
 {
     require_once TEMPLATEPATH . '/assets/includes/rotorwash-settings.php';
